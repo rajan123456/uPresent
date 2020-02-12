c14ace924330f62be68b8a0090a703e1d3c6524d
Rajan Gupta <rajan0493@gmail.com>
2020-02-12 09:37:51 +0800

    Fix Unit Test

--- user/src/test/java/com/upresent/user/UserApplicationTests.java
+++ user/src/test/java/com/upresent/user/UserApplicationTests.java
@@ -1,11 +1,12 @@
 package com.upresent.user;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
