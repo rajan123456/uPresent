7339ac617fb05a25d88ed052295cf15b9a207772
Ashish Gupta <ashishgupta@Ashishs-MacBook-Pro-2.local>
2020-03-02 21:19:28 +0800

    test case fix metrics

--- metrics/src/test/java/com/upresent/metrics/MetricsApplicationTests.java
+++ metrics/src/test/java/com/upresent/metrics/MetricsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.upresent.metrics;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+@RunWith(PowerMockRunner.class)
 @SpringBootTest
 class MetricsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@PrepareForTest(SpringApplication.class)
+	public void contextLoads() {
+		mockStatic(SpringApplication.class);
+		MetricsApplication.main(new String[]{"Hello", "World"});
+		verifyStatic(SpringApplication.class);
+		SpringApplication.run(MetricsApplication.class, "Hello", "World");
 	}
-
 }
