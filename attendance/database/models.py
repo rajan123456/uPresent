ec987b4fb720d1777749d87ae4562c718d477228
Ashish Gupta <ashishgupta@Ashishs-MacBook-Pro-2.local>
2020-03-11 20:48:15 +0800

    missed files

--- attendance/database/models.py
+++ attendance/database/models.py
@@ -1,10 +1,8 @@
 from .db import db
 import datetime
-from mongoengine import *
-
 
 
 class Attendance(db.Document):
     userId = db.StringField(required=True, unique=True)
-    location = PointField(required=True)
+    location = db.PointField(required=True)
     date_captured = db.DateTimeField(default=datetime.datetime.utcnow)
