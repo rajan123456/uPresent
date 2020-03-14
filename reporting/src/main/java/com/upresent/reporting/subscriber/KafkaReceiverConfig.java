24f03da8734525c55b59c809d51923c57a076232
Rajan Gupta <rajan0493@gmail.com>
2020-03-14 20:55:44 +0800

    Use correct deserializer

--- reporting/src/main/java/com/upresent/reporting/subscriber/KafkaReceiverConfig.java
+++ reporting/src/main/java/com/upresent/reporting/subscriber/KafkaReceiverConfig.java
@@ -1,8 +1,8 @@
 package com.upresent.reporting.subscriber;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.upresent.reporting.utils.Constants;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
