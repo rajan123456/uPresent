b0c866994cd5d81a07e5a7cc17b1d7df9f19d11a
Rajan Gupta <rajan0493@gmail.com>
2020-03-25 23:50:52 +0800

    Migrate to new dev box

--- uPresent/app/config/config.js
+++ uPresent/app/config/config.js
@@ -1,3 +1,3 @@
-export const baseUrlUserApi = 'http://54.83.79.180:8083/user';
-export const baseUrlFileApi = 'http://54.83.79.180:8083/file';
-export const baseUrlAuthApi = 'http://54.83.79.180:8083/auth';
+export const baseUrlUserApi = 'http://3.15.151.129:8083/user';
+export const baseUrlFileApi = 'http://3.15.151.129:8083/file';
+export const baseUrlAuthApi = 'http://3.15.151.129:8083/auth';
