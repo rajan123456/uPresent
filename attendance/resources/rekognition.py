7a1ec1cd6debabc180580dc27d66747e6f08b79c
Ashish Gupta <ashishgupta@Ashishs-MacBook-Pro-2.local>
2020-03-25 23:39:24 +0800

    rekognition flow tested

--- attendance/resources/rekognition.py
+++ attendance/resources/rekognition.py
@@ -3,21 +3,19 @@ from flask import current_app
 
 
 def compare_faces(targetId, sourceId):
-    return 'success'
     client = boto3.client('rekognition',
                           aws_access_key_id="test",
                           aws_secret_access_key="test",
                           region_name='us-west-2')
     imageSource = open(current_app.config['UPLOAD_DIR']+sourceId, 'rb')
     imageTarget = open(current_app.config['UPLOAD_DIR']+targetId, 'rb')
-
-    response = client.compare_faces(SimilarityThreshold=60,
+    response = client.compare_faces(SimilarityThreshold=current_app.config['THRESHOLD_CONFIDENCE'],
                                     SourceImage={'Bytes': imageSource.read()},
                                     TargetImage={'Bytes': imageTarget.read()})
-
+    similarity = '0'
     for faceMatch in response['FaceMatches']:
         similarity = str(faceMatch['Similarity'])
     imageSource.close()
     imageTarget.close()
-    if similarity > current_app.config['THRESHOLD_CONFIDENCE']:
+    if similarity == '0':
         raise Exception('Image mismatch found!')
