969fefb89960b2d6d60346bed66a49be17ed8926
Rajan Gupta <rajan0493@gmail.com>
2020-04-04 23:40:01 +0800

    Add getAll to geoFence

--- management/src/main/java/com/upresent/management/repository/GeoFenceRepository.java
+++ management/src/main/java/com/upresent/management/repository/GeoFenceRepository.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.upresent.management.entity.GeoFenceData;
 
+import java.util.List;
+
 @Repository
 public interface GeoFenceRepository extends MongoRepository<GeoFenceData, String> {
-	
+
 	GeoFenceData findByUniversityName(String universityName);
+
+	List<GeoFenceData> findAll();
 }
