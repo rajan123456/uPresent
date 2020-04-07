35241750fe57db0a8e6f397207724a385a59c2a6
Dipty Ojha <e0401948@u.nus.edu>
2020-04-07 12:36:02 +0800

    config file

--- videoprocessor/config.py
+++ videoprocessor/config.py
@@ -5,7 +5,7 @@ class Config(object):
     }
     VIDEO_INPUT_PATH = 'rtmp://3.15.151.129/app/'
     FRAMES_PATH = './data'
-    KAFKA_SERVER_IP = 'localhost:9092'
+    KAFKA_SERVER_IP = 'broker:29092'
     KAFKA_TOPIC = 'videoCollector'
     KAFKA_BATCH_SIZE = 176580  # this value should be as per the size of image frames
     KAFKA_LINGER_MS = 10
