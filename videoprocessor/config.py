0b67100cd38bbe5fa05347bd0aed4abd41ab24c2
Dipty Ojha <e0401948@u.nus.edu>
2020-04-07 12:34:03 +0800

    config file

--- videoprocessor/config.py
+++ videoprocessor/config.py
@@ -3,9 +3,9 @@ class Config(object):
         "SERVICE_NAME": "video-processor",
         "SERVER_URL": "http://apm-server:8200",
     }
-    VIDEO_INPUT_PATH = 'rtmp://3.15.151.129/app/key' # key needs to be set dynamically
-    FRAMES_PATH = './data' # change this path before running
-    KAFKA_SERVER_IP = 'broker:29092'
+    VIDEO_INPUT_PATH = 'rtmp://3.15.151.129/app/'
+    FRAMES_PATH = './data'
+    KAFKA_SERVER_IP = 'localhost:9092'
     KAFKA_TOPIC = 'videoCollector'
     KAFKA_BATCH_SIZE = 176580  # this value should be as per the size of image frames
     KAFKA_LINGER_MS = 10
