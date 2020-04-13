3ce3123515ade89f9993a2ea942c69d19bcc16c4
diptyojha <54563535+diptyojha@users.noreply.github.com>
2020-04-13 18:52:48 +0800

    Update config.py

--- streamProcessor/config.py
+++ streamProcessor/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     BATCH_DURATION = 3
     TOPIC = "videoCollector"
-    BROKER_INFO = "localhost:9092"
+    BROKER_INFO = "broker:29092"
     PYTHONPATH = "D:/ApacheSpark/spark-2.4.5-bin-hadoop2.7/python"
     SPARK_HOME = "D:/ApacheSpark/spark-2.4.5-bin-hadoop2.7"
     IMAGE_FOLDER_PATH = "D:/PythonWorkspace/"
