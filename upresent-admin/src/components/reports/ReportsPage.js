5d7f4da49abeb1d1121431531cd21f12e12fdb30
Rajan Gupta <rajan0493@gmail.com>
2020-04-16 22:20:57 +0800

    Add Results Table Initial

--- upresent-admin/src/components/reports/ReportsPage.js
+++ upresent-admin/src/components/reports/ReportsPage.js
@@ -5,10 +5,25 @@ import moment from "moment";
 import { getModules } from "../../api/moduleApi";
 import { getAttendanceReport } from "../../api/reportingApi";
 import { toast } from "react-toastify";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 function ReportsPage() {
   const [errors] = useState({});
   const [modules, setModules] = useState([]);
+  const [reportData, setReportData] = useState({});
   const [report, setReport] = useState({
     moduleCode: "",
     startDate: moment(new Date()).format("MM/DD/YYYY"),
@@ -59,10 +74,12 @@ function ReportsPage() {
       report.moduleCode
     ).then((_resp) => {
       if (_resp.message === "ok") {
-        toast.success("I got the data!");
+        setReportData(_resp.data);
       } else toast.warn("Something went wrong, please try again later.");
     });
   }
+
+  const classes = useStyles();
 
   return (
     <div className="container-fluid">
@@ -78,6 +95,35 @@ function ReportsPage() {
           onEndDateChange={handleDateChangeEndDate}
           onSubmit={handleSubmit}
         />
+        <hr />
+        <TableContainer component={Paper}>
+          <Table
+            className={classes.table}
+            size="small"
+            aria-label="Attendance Report"
+          >
+            <TableHead>
+              {reportData.dates &&
+                reportData.dates.map((date) => (
+                  <TableRow>
+                    <TableCell>Student</TableCell>
+                    <TableCell align="right">{date}</TableCell>
+                  </TableRow>
+                ))}
+            </TableHead>
+            <TableBody>
+              {reportData.dates &&
+                reportData.dates.map((date) => (
+                  <TableRow key={date}>
+                    <TableCell component="th" scope="row">
+                      {date}
+                    </TableCell>
+                    <TableCell align="right">{date}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
