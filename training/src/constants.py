933ec410c098b57acbf3ba71893df9fd81b4b361
Ashish Gupta <ashishgupta@Ashishs-MacBook.local>
2020-04-19 11:54:24 +0800

    change to scheduler

--- training/src/constants.py
+++ training/src/constants.py
@@ -1,13 +1,12 @@
-ELASTIC_APM = {
-    'SERVICE_NAME': 'attendance-service',
-    'SERVER_URL': 'http://apm-server:8200'
-}
-SAGA_ENABLED = 1
+# ELASTIC_APM = {
+#     'SERVICE_NAME': 'attendance-service',
+#     'SERVER_URL': 'http://apm-server:8200'
+# }
 USER_TRAINING_IMAGE_COUNT = 30
-FILE_SYSTEM_POLLING_DELAY_IN_SECONDS = 1
-# DATASET_PATH = '/app/resources/dataset'
-# PICKLE_FILES_DIR = '/app/resources/output'
-# MODEL_FILES_DIR = '/app/resources/model'
-DATASET_PATH = '/Users/anchitseth/Desktop/learn_vol/ml-data-vol/dataset'
-PICKLE_FILES_DIR = '/Users/anchitseth/Desktop/learn_vol/ml-data-vol/output'
-MODEL_FILES_DIR = '/Users/anchitseth/Desktop/learn_vol/ml-data-vol/model'
+FILE_SYSTEM_POLLING_DELAY_IN_SECONDS = 10
+DATASET_PATH = '/app/resources/dataset'
+PICKLE_FILES_DIR = '/app/resources/output'
+MODEL_FILES_DIR = '/app/resources/model'
+# DATASET_PATH = '/Users/ashishgupta/git/uPresent/scripts/dev/ml-data-vol/dataset'
+# PICKLE_FILES_DIR = '/Users/ashishgupta/git/uPresent/scripts/dev/ml-data-vol/output'
+# MODEL_FILES_DIR = '/Users/ashishgupta/git/uPresent/scripts/dev/ml-data-vol/model'
--- training/src/training_dataset_watchdog.py
+++ training/src/training_dataset_watchdog.py
@@ -4,6 +4,7 @@ import constants
 from training import train_model
 from embeddings import extract_embeddings
 import logging
+import schedule
 
 log = logging.getLogger('root')
 log.info("Watchdog starts.")
@@ -11,39 +12,47 @@ print("Watchdog starts.")
 students_image_count_obj = {}
 already_trained_students_count = -1
 
-try: 
-    while True: 
-        for username in os.listdir(constants.DATASET_PATH):
-            concatenated_dir = constants.DATASET_PATH + '/' + username
-            if os.path.isdir(concatenated_dir):
-                students_image_count_obj[username] = len([file for file in os.listdir(concatenated_dir) if (os.path.isfile(concatenated_dir + "/" + file) and os.path.splitext(file)[1].lower() == ".png")])
-        start_training = True
-        for username in students_image_count_obj.keys():
-            if students_image_count_obj[username] != constants.USER_TRAINING_IMAGE_COUNT:
-                start_training = False
-                break
-        # check if any new user is added, i.e. is there really any need of training again.
-        if start_training:
-            students_count = len(students_image_count_obj.keys())
-            if students_count > already_trained_students_count:
-                already_trained_students_count = students_count
+
+def job():
+    try:
+        while True:
+            for username in os.listdir(constants.DATASET_PATH):
+                concatenated_dir = constants.DATASET_PATH + '/' + username
+                if os.path.isdir(concatenated_dir):
+                    students_image_count_obj[username] = len([file for file in os.listdir(concatenated_dir) if (
+                                os.path.isfile(concatenated_dir + "/" + file) and os.path.splitext(file)[
+                            1].lower() == ".png")])
+            start_training = True
+            for username in students_image_count_obj.keys():
+                if students_image_count_obj[username] != constants.USER_TRAINING_IMAGE_COUNT:
+                    start_training = False
+                    break
+            # check if any new user is added, i.e. is there really any need of training again.
+            if start_training:
+                students_count = len(students_image_count_obj.keys())
+                if students_count > already_trained_students_count:
+                    already_trained_students_count = students_count
+                else:
+                    log.info("Training terminated to avoid redundant trainings.")
+                    print("Training terminated to avoid redundant trainings.")
+                    start_training = False
             else:
-                log.info("Training terminated to avoid redundant trainings.")
-                print("Training terminated to avoid redundant trainings.")
-                start_training = False
-        else:
-            log.info("Training didn't start because of insufficient number of images.")
-            print("Training didn't start because of insufficient number of images.")
-            log.info("Metadata: " + str(students_image_count_obj))
-            print("Metadata: " + str(students_image_count_obj))
-        if start_training:
-            log.info("Initialising model's training.")
-            print("Initialising model's training.")
-            extract_embeddings()
-            train_model()
-        time.sleep(constants.FILE_SYSTEM_POLLING_DELAY_IN_SECONDS) 
-except KeyboardInterrupt: 
-    log.error("Training process' polling job interrupted.")
-    print("Training process' polling job interrupted.")
+                log.info("Training didn't start because of insufficient number of images.")
+                print("Training didn't start because of insufficient number of images.")
+                log.info("Metadata: " + str(students_image_count_obj))
+                print("Metadata: " + str(students_image_count_obj))
+            if start_training:
+                log.info("Initialising model's training.")
+                print("Initialising model's training.")
+                extract_embeddings()
+                train_model()
+    except KeyboardInterrupt:
+        log.error("Training process' polling job interrupted.")
+        print("Training process' polling job interrupted.")
 
 
+schedule.every(constants.FILE_SYSTEM_POLLING_DELAY_IN_SECONDS).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
