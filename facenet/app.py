f0c03399a88aadafa3a2cfe3ce763190db72588c
Anchit Seth <anchitseth@Anchits-MBP.lan>
2020-04-19 23:14:10 +0800

    Fixes

--- facenet/app.py
+++ facenet/app.py
@@ -4,6 +4,8 @@ from flask_restful import Api
 from flask_restful_swagger import swagger
 from src.routes import initialize_routes
 import os
+import logging
+from src.custom_logger import LogHandler
 
 app = Flask(__name__)
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -14,7 +16,7 @@ api = swagger.docs(Api(app), apiVersion='0.1')
 # Initializing custom logger
 log = logging.getLogger('root')
 log.setLevel(logging.INFO)
-log.addHandler(log_config.LogHandler())
+log.addHandler(LogHandler())
 
 initialize_routes(api)
 app.run(host='0.0.0.0')
