77f5c34838dcd1bdbb9d17aea83a4235ffa6cb46
Dipty Ojha <e0401948@u.nus.edu>
2020-04-20 21:39:29 +0800

    azure face detect initial commit

---
+++ attendance/resources/azureface.py
@@ -0,0 +1,36 @@
+import os
+import logging
+
+from azure.cognitiveservices.vision.face import FaceClient
+from flask import current_app
+from msrest.authentication import CognitiveServicesCredentials
+
+KEY = current_app.config['AZURE_FACE_SUBSCRIPTION_KEY']
+ENDPOINT = current_app.config['AZURE_FACE_ENDPOINT']
+
+log = logging.getLogger('root')
+
+
+def compare_faces_azure(targetId, sourceId):
+    # Create an authenticated FaceClient.
+    face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+    image_source = open(current_app.config['UPLOAD_DIR']+sourceId, 'r+b')
+    image_target = open(current_app.config['UPLOAD_DIR']+targetId, 'r+b')
+
+    detected_faces_source = face_client.face.detect_with_stream(image_source)
+    # Add the returned face's face ID
+    source_image_id = detected_faces_source[0].face_id
+
+    detected_faces_target = face_client.face.detect_with_stream(image_target)
+    # Add the returned face's face ID
+    target_image_id = detected_faces_target[0].face_id
+
+    verify_result_same = face_client.face.verify_face_to_face(source_image_id, target_image_id)
+    print('Faces from {} & {} are of the same person, with confidence: {}'
+          .format(image_source, image_target, verify_result_same.confidence)
+          if verify_result_same.is_identical
+          else 'Faces from {} & {} are of a different person, with confidence: {}'
+          .format(image_source, image_target, verify_result_same.confidence))
+
+
