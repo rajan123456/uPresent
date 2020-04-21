e157bf0ab57e2d8a72540c591ffa67b587064660
Ashish Gupta <ashishgupta@Ashishs-MacBook.local>
2020-04-21 18:30:04 +0800

    api change+srs http callback

--- videoprocessor/resources/videoProcessor.py
+++ videoprocessor/resources/videoProcessor.py
@@ -2,6 +2,7 @@ from flask import Response, request
 from flask_restful_swagger import swagger
 from resources.videoSplitter import videosplitter
 from flask_restful import Resource
+from threading import Thread
 
 
 class VideoProcessorApi(Resource):
@@ -9,9 +10,5 @@ class VideoProcessorApi(Resource):
     @swagger.operation()
     def post(self):
         body = request.get_json()
-        try:
-            videosplitter(body['key'])
-        except Exception as ex:
-            return {'message': str(ex)}, 500
+        Thread(target=videosplitter(body['stream'])).start()
         return Response({'Video pushed to Kafka successfully...'}, status=200)
-
