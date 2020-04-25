4098505167a5c2786311243b5cf4a76ca27a02df
Rajan Gupta <rajan0493@gmail.com>
2020-04-25 16:40:29 +0800

    Point AdminUI to Prod

--- upresent-admin/src/config/config.js
+++ upresent-admin/src/config/config.js
@@ -3,26 +3,31 @@ export const toastDuration = 3000;
 
 export const baseUrlUserApi =
   // environmentVariable === "prod"
-  //   ? "https://user.upresent.ga/user"
-  //   :
-  "https://dev.upresent.ga/user/user";
+  //   ?
+  "https://user.upresent.ga/user";
+//   :
+//"https://dev.upresent.ga/user/user";
 export const baseUrlAuthApi =
   // environmentVariable === "prod"
-  //   ? "https://user.upresent.ga/auth"
-  //   :
-  "https://dev.upresent.ga/user/auth";
+  //   ?
+  "https://user.upresent.ga/auth";
+//   :
+//"https://dev.upresent.ga/user/auth";
 export const baseUrlFenceApi =
   // environmentVariable === "prod"
-  //   ? "https://management.upresent.ga/manage/geo-fence"
-  //   :
-  "https://dev.upresent.ga/management/manage/geo-fence";
+  //   ?
+  "https://management.upresent.ga/manage/geo-fence";
+//   :
+//"https://dev.upresent.ga/management/manage/geo-fence";
 export const baseUrlModuleApi =
   // environmentVariable === "prod"
-  //   ? "https://management.upresent.ga/manage/module"
-  //   :
-  "https://dev.upresent.ga/management/manage/module";
+  //   ?
+  "https://management.upresent.ga/manage/module";
+//   :
+//"https://dev.upresent.ga/management/manage/module";
 export const baseUrlReportingApi =
   // environmentVariable === "prod"
-  //   ? "https://reporting.upresent.ga/attendance"
-  //   :
-  "https://dev.upresent.ga/reporting/attendance";
+  //   ?
+  "https://reporting.upresent.ga/attendance";
+//   :
+//"https://dev.upresent.ga/reporting/attendance";
