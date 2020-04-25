62e7d31d7123f74f1d324617f219be7499e5f495
Dipty Ojha <e0401948@u.nus.edu>
2020-04-25 13:00:11 +0800

    Modified for structured streaming

--- streamProcessor/config.py
+++ streamProcessor/config.py
@@ -8,3 +8,8 @@ class Config(object):
     MIN_HEIGHT = 3
     MIN_WIDTH = 3
     IMAGE_COUNT = 60
+    SPARK_JARS_PATH = "file:///streamProcessor//spark-sql-kafka-0-10_2.11-2.4.5.jar," \
+                      "file:///streamProcessor//kafka-clients-2.4.0.jar "
+
+    SPARK_EXECUTOR_JARS = "file:///streamProcessor//spark-sql-kafka-0-10_2.11-2.4.5.jar" \
+                          ":file:///streamProcessor//kafka-clients-2.4.0.jar"
--- streamProcessor/videoStreamCollector.py
+++ streamProcessor/videoStreamCollector.py
@@ -1,80 +1,89 @@
 import os
-import json
 import base64
 import logging
 import config
-
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 from resources import face_scrapper
 
 
-
 def init_spark():
-    sc = SparkContext(appName="videoStreamCollector")
-    ssc = StreamingContext(sc, config.Config.BATCH_DURATION)
-    sc.setLogLevel("WARN")
-    return sc, ssc
-
-
-def process(rdd, sc):
-    try:
-        spark = SparkSession(sc)
-        if not rdd.isEmpty():
-            # Converting DStream into DataFrames
-            df = spark.read.json(rdd)
-            df.show()
-            df.createOrReplaceTempView("images")
-            if not df.count == 0:
-                imageDataFrame = spark.sql("select imageData, username from images")
-                imageDataFrame.show()
-
-                # Getting imageBase64 from SQL and storing them into filesystem
-                for imageInfo in imageDataFrame.collect():
-                    image_folder = config.Config.IMAGE_FOLDER_PATH + imageInfo.username
-
-                    # Creating image folder based on userNames
-                    if not os.path.exists(image_folder):
-                        os.makedirs(image_folder)
-                        image_no = 1
-                    else:
-                        image_no = len(os.listdir(image_folder)) + 1
-
-                    if len(os.listdir(image_folder)) < config.Config.IMAGE_COUNT:
-                        # Converting Base64 to image
-                        with open(image_folder + "/imageToSave" + str(image_no) + ".png", "wb") as fh:
-                            image = base64.b64decode(imageInfo.imageData)
-
-                            # Calling face scrapper class to detect faces in the image
-                            face_len = face_scrapper.face_detection(image)
-                            if face_len == 1:
-                                fh.write(image)
-                                image_no += 1
-        else:
-            logging.warning("StreamProcessor did not receive any new data to process.")
-    except Exception as ex:
-        logging.warning('Exception while processing images inside process method--->>')
-        logging.warning(str(ex))
-        pass
+    spark = SparkSession \
+        .builder \
+        .appName("videoStreamCollector") \
+        .master("master") \
+        .config("spark.jars", config.Config.SPARK_JARS_PATH) \
+        .config("spark.executor.extraClassPath", config.Config.SPARK_EXECUTOR_JARS) \
+        .config("spark.executor.extraLibrary", config.Config.SPARK_EXECUTOR_JARS) \
+        .config("spark.driver.extraClassPath", config.Config.SPARK_EXECUTOR_JARS) \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    return spark
 
 
 def main():
-    sc, ssc = init_spark()
+    spark = init_spark()
     topic = config.Config.TOPIC
     brokers = config.Config.BROKER_INFO
 
-    # Creating spark DStreams
-    stream = KafkaUtils.createDirectStream(
-        ssc, [topic], {"metadata.broker.list": brokers})
-    lines = stream.map(lambda x: json.loads(x[1]))
-    lines.pprint()
-    lines.foreachRDD(lambda rdd: process(rdd, sc))
-    logging.warning("Spark DStream created from Kafka stream input")
+    df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", brokers) \
+        .option("subscribe", topic) \
+        .option("startingOffsets", "latest") \
+        .load()
+    df1 = df.selectExpr("CAST(value AS STRING)", "timestamp")
+
+    transaction_detail_schema = StructType() \
+        .add("imageData", StringType()) \
+        .add("username", StringType()) \
+        .add("timestamp", StringType())
+
+    df2 = df1 \
+        .select(from_json(col("value"), transaction_detail_schema).alias("transaction_detail"), "timestamp")
+    df3 = df2.select("transaction_detail.*", "timestamp")
+    df3.printSchema()
+    df3.writeStream.trigger(processingTime='1 seconds').outputMode("append") \
+        .format("console") \
+        .start()
+
+    if not df3.count == 0:
+        df4 = df3.select("imageData", "username")
+        trans_detail_write_stream = df4 \
+            .writeStream \
+            .trigger(processingTime='1 seconds') \
+            .format("memory") \
+            .queryName("images") \
+            .outputMode("append") \
+            .start()
+
+        imageDataFrame = spark.sql("select * from images")
+        imageDataFrame.show()
+        for imageInfo in imageDataFrame.collect():
+            image_folder = config.Config.IMAGE_FOLDER_PATH + imageInfo.username
+
+            # Creating image folder based on userNames
+            if not os.path.exists(image_folder):
+                os.makedirs(image_folder)
+                image_no = 1
+            else:
+                image_no = len(os.listdir(image_folder)) + 1
+
+            if len(os.listdir(image_folder)) < 60:
+                # Converting Base64 to image
+                with open(image_folder + "/imageToSave" + str(image_no) + ".png", "wb") as fh:
+                    image = base64.b64decode(imageInfo.imageData)
+
+                    # Calling face scrapper class to detect faces in the image
+                    face_len = face_scrapper.face_detection(image)
+                    if face_len == 1:
+                        fh.write(image)
+                        image_no += 1
+    else:
+        logging.warning("Video Stream has no  data to process")
     # To start the spark streaming
-    ssc.start()
-    ssc.awaitTermination()
+    trans_detail_write_stream.awaitTermination()
 
 
 if __name__ == '__main__':
