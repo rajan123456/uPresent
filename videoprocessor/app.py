b12e62b09545c0471383f37d0e23e5e529492d22
Ashish Gupta <ashishgupta@Ashishs-MacBook.local>
2020-04-25 20:40:44 +0800

    route added to main

--- videoprocessor/app.py
+++ videoprocessor/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from elasticapm.contrib.flask import ElasticAPM
 from flask_restful_swagger import swagger
 from resources import custom_logger
+from resources.routes import initialize_routes
 import logging
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ log.addHandler(custom_logger.LogHandler())
 apm = ElasticAPM(app)
 api = Api(app)
 api = swagger.docs(Api(app), apiVersion='0.1')
+initialize_routes(api)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
