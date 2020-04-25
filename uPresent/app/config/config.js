a60ae725939eb1521c8446b485db085a4a251830
Rajan Gupta <rajan0493@gmail.com>
2020-04-25 15:35:45 +0800

    Prod Microservices Kubernetes

--- uPresent/app/config/config.js
+++ uPresent/app/config/config.js
@@ -1,8 +1,17 @@
-export const baseUrlUserApi = 'https://dev.upresent.ga/user/user';
-export const baseUrlFileApi = 'https://dev.upresent.ga/user/file';
-export const baseUrlAuthApi = 'https://dev.upresent.ga/user/auth';
-export const baseUrlModuleApi =
-  'https://dev.upresent.ga/management/student-module';
+//DEV
+// export const baseUrlUserApi = 'https://dev.upresent.ga/user/user';
+// export const baseUrlFileApi = 'https://dev.upresent.ga/user/file';
+// export const baseUrlAuthApi = 'https://dev.upresent.ga/user/auth';
+// export const baseUrlModuleApi =
+//   'https://dev.upresent.ga/management/student-module';
+// export const baseUrlAttendanceApi =
+//   'https://dev.upresent.ga/attendance/api/attendance';
+// export const baseUrlRtmp = 'rtmp://dev.upresent.ga/app/';
+
+export const baseUrlUserApi = 'https://user.upresent.ga/user';
+export const baseUrlFileApi = 'https://user.upresent.ga/file';
+export const baseUrlAuthApi = 'https://user.upresent.ga/auth';
+export const baseUrlModuleApi = 'https://management.upresent.ga/student-module';
 export const baseUrlAttendanceApi =
-  'https://dev.upresent.ga/attendance/api/attendance';
-export const baseUrlRtmp = 'rtmp://dev.upresent.ga/app/';
+  'https://attendance.upresent.ga/api/attendance';
+export const baseUrlRtmp = 'rtmp://srs.upresent.ga/app/';
