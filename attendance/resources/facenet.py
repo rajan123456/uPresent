94096df9f82283a1ad63a0db29eb480402410132
Ashish Gupta <ashishgupta@Ashishs-MacBook.local>
2020-04-27 01:14:57 +0800

    add logs

--- attendance/resources/facenet.py
+++ attendance/resources/facenet.py
@@ -8,7 +8,7 @@ import os
 log = logging.getLogger('root')
 
 def compare_faces_facenet(targetId, username):
-    log.info("Trying to compare faces for student attendance with FaceNet ---->>")
+    log.info("Trying to compare faces for student attendance with FaceNet for username ---->>"+ username)
     facenet_api = os.getenv('FACENET_RECOGNITION_API')
     if facenet_api is None:
         facenet_api = current_app.config['FACENET_RECOGNITION_API']
@@ -18,8 +18,9 @@ def compare_faces_facenet(targetId, username):
         log.error('No data received from face recgnition service')
         raise Exception('No data found from face recgnition service')
     else:
-        log.info(" Facial recognition with FaceNet confidence rating is" + str(facenetApiData.get('confidence')))
+        log.info(" Facial recognition with FaceNet username and confidence rating is" + facenetApiData.get('username')
+                 + str(facenetApiData.get('confidence')))
     if facenetApiData.get('username') != username or \
             facenetApiData.get('confidence') < current_app.config['THRESHOLD_CONFIDENCE']:
-        log.error('image mismatch found due to low confidence')
+        log.error('image mismatch found due to low confidence or username')
         raise Exception('Image mismatch found!')
