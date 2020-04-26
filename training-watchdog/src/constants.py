3cbb42c39753201419ff740ad188619ccf3808cc
Rajan Gupta <rajan0493@gmail.com>
2020-04-26 23:26:33 +0800

    Change USER_TRAINING_IMAGE_COUNT to 60

--- training-watchdog/src/constants.py
+++ training-watchdog/src/constants.py
@@ -1,4 +1,4 @@
-USER_TRAINING_IMAGE_COUNT = 30
+USER_TRAINING_IMAGE_COUNT = 60
 FILE_SYSTEM_POLLING_DELAY_IN_SECONDS = 2
 # not really a constant, it's value changes at runtime as and when data of new students is trained
 ALREADY_TRAINED_STUDENTS_COUNT = -1
