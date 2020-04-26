e7f56a702964062684b2b6a346a911a7239c7e1c
Rajan Gupta <rajan0493@gmail.com>
2020-04-26 19:55:06 +0800

    Revert to Dev

--- uPresent/app/config/config.js
+++ uPresent/app/config/config.js
@@ -1,17 +1,18 @@
 //DEV
-// export const baseUrlUserApi = 'https://dev.upresent.ga/user/user';
-// export const baseUrlFileApi = 'https://dev.upresent.ga/user/file';
-// export const baseUrlAuthApi = 'https://dev.upresent.ga/user/auth';
-// export const baseUrlModuleApi =
-//   'https://dev.upresent.ga/management/student-module';
-// export const baseUrlAttendanceApi =
-//   'https://dev.upresent.ga/attendance/api/attendance';
-// export const baseUrlRtmp = 'rtmp://dev.upresent.ga/app/';
-
-export const baseUrlUserApi = 'https://user.upresent.ga/user';
-export const baseUrlFileApi = 'https://user.upresent.ga/file';
-export const baseUrlAuthApi = 'https://user.upresent.ga/auth';
-export const baseUrlModuleApi = 'https://management.upresent.ga/student-module';
+export const baseUrlUserApi = 'https://dev.upresent.ga/user/user';
+export const baseUrlFileApi = 'https://dev.upresent.ga/user/file';
+export const baseUrlAuthApi = 'https://dev.upresent.ga/user/auth';
+export const baseUrlModuleApi =
+  'https://dev.upresent.ga/management/student-module';
 export const baseUrlAttendanceApi =
-  'https://attendance.upresent.ga/api/attendance';
-export const baseUrlRtmp = 'rtmp://srs.upresent.ga/app/';
+  'https://dev.upresent.ga/attendance/api/attendance';
+export const baseUrlRtmp = 'rtmp://dev.upresent.ga/app/';
+
+//PROD-MICRO
+// export const baseUrlUserApi = 'https://user.upresent.ga/user';
+// export const baseUrlFileApi = 'https://user.upresent.ga/file';
+// export const baseUrlAuthApi = 'https://user.upresent.ga/auth';
+// export const baseUrlModuleApi = 'https://management.upresent.ga/student-module';
+// export const baseUrlAttendanceApi =
+//   'https://attendance.upresent.ga/api/attendance';
+// export const baseUrlRtmp = 'rtmp://srs.upresent.ga/app/';
