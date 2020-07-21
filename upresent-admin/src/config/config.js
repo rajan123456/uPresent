1f367f4e018e75e2c2f391c9fd39192bd1b68ff1
Rajan Gupta <rajan0493@gmail.com>
2020-07-21 21:31:30 +0800

    Remove unused css

--- upresent-admin/src/config/config.js
+++ upresent-admin/src/config/config.js
@@ -4,38 +4,42 @@ export const toastDuration = 3000;
 export const baseUrlUserApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://user.upresent.ga/user";
-  //   :
-  "http://localhost:8084/user";
+  "https://user.upresent.ga/user";
+//   :
+//"http://localhost:8084/user";
 export const baseUrlAuthApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://user.upresent.ga/auth";
-  //   :
-  "http://localhost:8084/auth";
+  "https://user.upresent.ga/auth";
+//   :
+//"http://localhost:8084/auth";
 export const baseUrlFileApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://user.upresent.ga/file";
-  //   :
-  "http://localhost:8084/file";
+  "https://user.upresent.ga/file";
+//   :
+//"http://localhost:8084/file";
 export const baseUrlSchoolApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://management.upresent.ga/manage/school";
-  //   :
-  "http://localhost:8082/manage/school";
+  "https://management.upresent.ga/manage/school";
+//   :
+//"http://localhost:8082/manage/school";
 export const baseUrlModuleApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://management.upresent.ga/manage/module";
-  //   :
-  "http://localhost:8082/manage/module";
+  "https://management.upresent.ga/manage/module";
+//   :
+//"http://localhost:8082/manage/module";
 export const baseUrlReportingApi =
   // environmentVariable === "prod"
   //   ?
-  //"https://reporting.upresent.ga/attendance";
-  //   :
-  "http://localhost:8083/attendance";
-
-export const baseUrlAttendanceApi = "http://localhost:5000/api/attendance";
+  "https://reporting.upresent.ga/attendance";
+//   :
+//"http://localhost:8083/attendance";
+export const baseUrlAttendanceApi =
+  // environmentVariable === "prod"
+  //   ?
+  "https://attendance.upresent.ga/api/attendance";
+//   :
+//"http://localhost:5000/api/attendance";
